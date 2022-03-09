16074596c8cd4b8ba1730444c5d9bfb4dfbbe66d
Dmitry <libafer99@gmail.com>
2022-03-09 18:51:23 +0500

    feat: add styles

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,3 @@
-import colors from 'vuetify/es5/util/colors';
-
 export default {
     // Target: https://go.nuxtjs.dev/config-target
     target: 'static',
@@ -59,18 +57,23 @@ export default {
     // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
     vuetify: {
         customVariables: ['~/assets/variables.scss'],
+        breakpoint: {
+            thresholds: {
+                xs: 0,
+                sm: 375,
+                md: 768,
+                lg: 1230,
+            },
+        },
         theme: {
-            dark: true,
             themes: {
-                dark: {
-                    primary: colors.blue.darken2,
-                    accent: colors.grey.darken3,
-                    secondary: colors.amber.darken3,
-                    info: colors.teal.lighten1,
-                    warning: colors.amber.base,
-                    error: colors.deepOrange.accent4,
-                    success: colors.green.accent3,
-                },
+                primary: '#18a452',
+                accent: '#18a452',
+                secondary: '#2b3034',
+                warning: '#ff8a00',
+                error: '#e86565',
+                success: '#18a452',
+                tertiary: '#828282',
             },
         },
     },
